ef47afd42dc8b26855e57049308093824d900ff7
BingusEx <10480438+BingusEx@users.noreply.github.com>
2025-02-20 02:42:35 +0100

    Move order

--- src/UI/Windows/GTSInfo.cpp
+++ src/UI/Windows/GTSInfo.cpp
@@ -62,11 +62,11 @@ namespace GTS {
         const std::string pText = fmt::format("{}{}", sReal, sScale);
         ImUtil::CenteredProgress(CurrentScale / MaxScale, ImVec2(hasFlag(a_featureFlags, GTSInfoFeatures::kAutoSize) ? 0.0f : 160.0f, 0.0f), pText.c_str());
 
+        if (hasFlag(a_featureFlags, GTSInfoFeatures::kShowMaxSizePotion))
+            ImGui::Text("Bonus Size: %.2fx", _BonusSize);
+
         if (hasFlag(a_featureFlags, GTSInfoFeatures::kShowMaxSize))
 				ImGui::Text("Max Scale: %.2fx", MaxScale);
-
-        if (hasFlag(a_featureFlags, GTSInfoFeatures::kShowMaxSizePotion))
-        		ImGui::Text("Bonus Size: %.2fx", _BonusSize);
 
         if (a_Actor->formID == 0x14) {
             if (hasFlag(a_featureFlags, GTSInfoFeatures::kShowPermanentSize))
